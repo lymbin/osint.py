00b6376479237429025dbc8654030c1340a4aa8e
Dmitrii Kilchanov <sochineniya@yandex.ru>
2021-04-25 19:11:32 +0300

    Added normalize to package in search
    Fixed config.ini

--- search/search.py
+++ search/search.py
@@ -22,12 +22,21 @@ class Search:
     def __init__(self):
         self.config = Configuration()
 
+    def normalize(self, package: str) -> str:
+        return package.lower()
+
     def cve_search(self, cve_search_str: str) -> str:
         print('Scanning %s' % cve_search_str)
-        cve_search_bin = os.path.join(os.path.realpath(self.config.get_cve_path()), cve_bin)
+
+        cve_search_path = self.config.get_cve_path()
+        if os.path.isabs(cve_search_path):
+            cve_search_bin = os.path.join(os.path.realpath(cve_search_path), cve_bin)
+        else:
+            cve_search_bin = os.path.join(os.path.dirname(os.path.realpath(__file__)), cve_search_path, cve_bin)
+
         result = subprocess.run([cve_search_bin, '-p', cve_search_str], stdout=subprocess.PIPE)
         return result.stdout.decode('utf-8')
 
     def search(self, package: str, ver: str) -> str:
-        cve_search_str = ("%s:%s" % (package, ver))
+        cve_search_str = ("%s:%s" % (self.normalize(package), ver))
         return self.cve_search(cve_search_str)
